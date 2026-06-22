8976e325e83c2aa6ca88f37b01ce292c68a418db
Abhishek Yadav <yadavabhishek07258@gmail.com>
2026-06-22 14:39:43 +0530

    fix: tailwind v4 reinstall

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,3 @@
 //     'bg-gray-950', 'bg-white', 'bg-gray-800', 'bg-gray-100',
 //   ]
 // }
-
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class',
-  safelist: [
-    'text-sm', 'font-medium', 'text-indigo-500',
-    'text-gray-400', 'text-white', 'text-gray-600',
-    'text-gray-900', 'hidden', 'md:flex', 'md:hidden',
-    'bg-gray-950', 'bg-white', 'bg-gray-800', 'bg-gray-100',
-    'text-yellow-400', 'text-indigo-600', 'backdrop-blur-md',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
